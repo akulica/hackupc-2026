5d5cf6d8f94a7bfd7efcbfdb72aaad066329ed9e
akulica <152804681+akulica@users.noreply.github.com>
2026-04-25 15:25:21 +0200

    Add files via upload

--- scripts/note_spawner.gd
+++ scripts/note_spawner.gd
@@ -1,5 +1,6 @@
 extends Node2D
-
+var array = [1,2,4,0,1,2,3,4,1,3,2,4,1,2,3,2,2,3,4]
+var notenum = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position=Vector2(960,540)
@@ -8,25 +9,25 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.qwqq
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("notew"):
-		var node = preload("res://note.tscn").instantiate()
-		node.dir = Vector2(sin(PI),cos(PI))
-		add_child(node)
-	if Input.is_action_just_pressed("noteq"):
-		var node = preload("res://note.tscn").instantiate()
-		node.dir = Vector2(sin(-PI*3/5),cos(-PI*3/5))
-		add_child(node)
-		
+		instantiate_note(0)
 	if Input.is_action_just_pressed("notee"):
-		var node = preload("res://note.tscn").instantiate()
-		node.dir = Vector2(sin(PI*3/5),cos(PI*3/5))
-		add_child(node)
-	
+		instantiate_note(1)
 	if Input.is_action_just_pressed("noted"):
-		var node = preload("res://note.tscn").instantiate()
-		node.dir = Vector2(sin(PI*1/5),cos(PI*1/5))
-		add_child(node)
-	
+		instantiate_note(2)
 	if Input.is_action_just_pressed("notea"):
-		var node = preload("res://note.tscn").instantiate()
-		node.dir = Vector2(sin(-PI*1/5),cos(-PI*1/5))
-		add_child(node)
+		instantiate_note(3)
+	if Input.is_action_just_pressed("noteq"):
+		instantiate_note(4)
+	
+func instantiate_note(button: int) -> void:
+	var node = preload("res://note.tscn").instantiate()
+	node.dir = -Vector2(sin(PI-(float(button)*2./5.*PI)),cos(((float(button)*2./5.*PI))))
+	add_child(node)
+
+
+func _on_timer_timeout() -> void:
+	instantiate_note(array[notenum])
+	if notenum<array.size()-1:
+		notenum+=1
+	else:
+		notenum=0
